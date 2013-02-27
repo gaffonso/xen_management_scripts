665ab514b5e18245f2f3ffa0fa49e69595c6a170
Gary Affonso <gary@teahousesoftware.com>
2013-02-26 20:15:17 -0800

    xtrace mode

--- xen_create_vm.sh
+++ xen_create_vm.sh
@@ -3,14 +3,14 @@
 # run with:
 #   bash -c "$(curl -fsSL http://raw.github.com/5043824)" vm_name
 
-set -o verbose
-
 if [ $# -ne 1 -o "x$0" == "x"  -o $0 == "bash" ]; then
   echo "Missing VM name.  Supply that as first argument"
   exit 1
 fi
 
 VM_NAME=$1
+
+set -o xtrace
 
 lvcreate -L100G -n $VM_NAME stor      # 100gb storage volume in volume group "stor"
 cp /stor/vms/stacklet_images/centos.6-3.x86-64.20120709.img /stor/vms/${VM_NAME}_centos.6-3.x86-64.20120709.img
