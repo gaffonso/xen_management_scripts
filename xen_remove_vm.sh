8d4885c0b871c8fd276afe07c37a04f9a2372046
Gary Affonso <gary@teahousesoftware.com>
2013-02-27 10:33:42 -0800

    Fix arg problem (change the way curl is calling the script)

--- xen_remove_vm.sh
+++ xen_remove_vm.sh
@@ -3,14 +3,11 @@
 # run with:
 #   curl -s https://raw.github.com/gaffonso/xen_management_scripts/master/xen_remove_vm.sh | bash -s vm_name
 
-echo $0
-echo $1
+if [ $# -ne 1 ]; then
+  echo "Missing VM name."
+  exit 1
+fi
+VM_NAME=$1
 
-#if [ $# -ne 1 -o "x$0" == "x"  -o $0 == "bash" ]; then
-#  echo "Missing VM name."
-#  exit 1
-#fi
-#VM_NAME=$1
-#
-#xm destroy $VM_NAME-cos63
-#lvremove -f /dev/stor/$VM_NAME
+xm destroy $VM_NAME-cos63
+lvremove -f /dev/stor/$VM_NAME
