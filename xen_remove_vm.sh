d950ba8ec53265fe25ac5c4a4798fdd2f7a21250
Gary Affonso <gary@teahousesoftware.com>
2013-02-27 10:32:03 -0800

    Troubleshooting problem with $0 vs $1

--- xen_remove_vm.sh
+++ xen_remove_vm.sh
@@ -2,13 +2,9 @@
 #
 # run with:
 #   curl -s https://raw.github.com/gaffonso/xen_management_scripts/master/xen_remove_vm.sh | bash -s vm_name
-# alternatively:
-#   bash -c "$(curl -fsSL https://raw.github.com/gaffonso/xen_management_scripts/master/xen_remove_vm.sh)" vm_name
-# TODO: get script running via github
-
-# if called via curl, the vm_name (first argument) is $0, else $1
 
 echo $0
+echo $1
 
 #if [ $# -ne 1 -o "x$0" == "x"  -o $0 == "bash" ]; then
 #  echo "Missing VM name."
