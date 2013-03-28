f03af02c196fec9a52ecb9d995a7dd6830e2908a
Gary Affonso <gary@teahousesoftware.com>
2013-03-27 17:29:45 -0700

    Remove linux update and config commands (they are now in a separate script).  Move Java installation to first in the list as its the most brittle.

--- configure_vm_java7-mysql55.sh
+++ configure_vm_java7-mysql55.sh
@@ -1,28 +1,27 @@
 #!/bin/sh
 #
 # run with:
-#   curl -s https://raw.github.com/gaffonso/xen_management_scripts/master/configure_vm_java7-mysql55.sh | bash -s hostname mysql_root_password
+#   curl -s https://raw.github.com/gaffonso/xen_management_scripts/master/configure_vm_java7-mysql55.sh | bash -s mysql_root_password
 #
 # Note: Script assumes
 # - vm was created using the xen_create_vm.sh script (or equivalent)
+# - the script "update_and_configure_new_vm.sh" has been run inside this vm
 # - logged-in as root
 
 #set -o xtrace
 
 # check params
-if [ $# -ne 2 ]; then
-  echo "Missing hostname and/or mysql_root_password."
+if [ $# -ne 1 ]; then
+  echo "Missing mysql_root_password."
   exit 1
 fi
+MYSQL_ROOT_PASSWORD=$1
 
-HOSTNAME=$1
-MYSQL_ROOT_PASSWORD=$2
 
 echo
 echo "--------------------------"
 echo "Installing and Configuring"
 echo "--------------------------"
-echo "HOSTNAME: $HOSTNAME"
 echo "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD"
 echo
 
@@ -34,31 +33,18 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 
-# update system
-yum -y update
-
-
-# set timezone
-rm -f /etc/localtime
-ln -s /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-sed -i 's|ZONE="America/New York"|ZONE="America/Los_Angeles"|' /etc/sysconfig/clock
-
-
-# change hostname
-hostname $HOSTNAME
-echo "HOSTNAME=\"${HOSTNAME}\"" >> /etc/sysconfig/network
-
-
-# format and mount the storage volume
-mkfs.ext3 /dev/xvdb
-mkdir /stor
-mount /dev/xvdb /stor
-echo '/dev/xvdb       /stor       ext3    errors=remount-ro       0 0' >> /etc/fstab
-
-
 # prep some folders on the storage volume
 mkdir /stor/downloads
 mkdir /stor/project_roots
+
+
+# Install Java 7
+wget --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "https://edelivery.oracle.com/otn-pub/java/jdk/7u17-b02/jdk-7u17-linux-x64.rpm" -O /stor/downloads/jdk-7u17-linux-x64.rpm
+rpm -Uvh /stor/downloads/jdk-7u17-linux-x64.rpm
+touch /etc/profile.d/java.sh
+echo 'export JAVA_HOME=/usr/java/default' >> /etc/profile.d/java.sh
+echo 'export PATH=${JAVA_HOME}/bin:${PATH}' >> /etc/profile.d/java.sh
+source /etc/profile.d/java.sh
 
 
 # install subversion (1.6)
@@ -85,15 +71,6 @@ EOF
 mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql -u root -p$MYSQL_ROOT_PASSWORD mysql
 
 
-# Install Java 7
-wget --no-check-certificate --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" "https://edelivery.oracle.com/otn-pub/java/jdk/7u17-b02/jdk-7u17-linux-x64.rpm" -O /stor/downloads/jdk-7u17-linux-x64.rpm
-rpm -Uvh /stor/downloads/jdk-7u17-linux-x64.rpm
-touch /etc/profile.d/java.sh
-echo 'export JAVA_HOME=/usr/java/default' >> /etc/profile.d/java.sh
-echo 'export PATH=${JAVA_HOME}/bin:${PATH}' >> /etc/profile.d/java.sh
-source /etc/profile.d/java.sh
-
-
 # install grails 2.0.1
 wget http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.0.1.zip -P /stor/downloads/
 unzip /stor/downloads/grails-2.0.1.zip -d /usr/local
@@ -115,6 +92,7 @@ echo "export MAVEN_HOME=$M2_HOME" >> /etc/profile.d/maven.sh
 echo 'export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=256m"' >> /etc/profile.d/maven.sh
 echo "export PATH=${M2_HOME}/bin:${PATH}" >> /etc/profile.d/maven.sh
 source /etc/profile.d/maven.sh
+
 
 echo
 echo "--------------"
