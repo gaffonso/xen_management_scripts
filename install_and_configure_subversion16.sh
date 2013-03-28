39aa0217cbbcda7b095d553b2184075904cbb7c2
Gary Affonso <gary@teahousesoftware.com>
2013-03-28 12:44:30 -0700

    subversion 1.6 install/config script

---
+++ install_and_configure_subversion16.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# run with:
+#   curl -s https://raw.github.com/gaffonso/xen_management_scripts/master/install_and_configure_subversion16.sh | bash -s
+#
+# Note: Script assumes
+# - vm was created using the xen_create_vm.sh script (or equivalent)
+# - the script "update_and_configure_new_vm.sh" has been run inside this vm
+# - logged-in as root
+
+#set -o xtrace
+
+
+# Must be root to run this script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+echo
+echo "-------------------------------------------"
+echo "Installing and Configuring Subversion 1.6"
+echo "-------------------------------------------"
+echo
+
+# install subversion (1.6)
+yum -y install subversion
+
+echo
+echo "--------------"
+echo "Setup Complete"
+echo "--------------"
+echo
+svn --version
