aee47933291316380a27120aff0d7120ee0d49af
Gary Affonso <gary@teahousesoftware.com>
2013-03-28 12:31:25 -0700

    grails201 install/config script

---
+++ install_and_configure_grails201.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# run with:
+#   curl -s https://raw.github.com/gaffonso/xen_management_scripts/master/install_and_configure_grails201.sh | bash -s
+#
+# Note: Script assumes
+# - vm was created using the xen_create_vm.sh script (or equivalent)
+# - the script "update_and_configure_new_vm.sh" has been run inside this vm
+# - logged-in as root
+
+#set -o xtrace
+
+# Must be root to run this script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+echo
+echo "---------------------------------------"
+echo "Installing and Configuring Grails 2.0.1"
+echo "---------------------------------------"
+echo
+
+
+# create download folder if necessary
+[ ! -f /stor/downloads ] && mkdir /stor/downloads
+
+
+# install grails 2.0.1
+wget http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.0.1.zip -P /stor/downloads/
+unzip /stor/downloads/grails-2.0.1.zip -d /usr/local
+ln -s /usr/local/grails-2.0.1 /usr/local/grails
+touch /etc/profile.d/grails.sh
+echo 'export GRAILS_HOME=/usr/local/grails' >> /etc/profile.d/grails.sh
+source /etc/profile.d/grails.sh
+echo "export PATH=${GRAILS_HOME}/bin:${PATH}" >> /etc/profile.d/grails.sh
+source /etc/profile.d/grails.sh
+
+
+echo
+echo "--------------"
+echo "Setup Complete"
+echo "--------------"
+echo
+echo "GRAILS_HOME: $GRAILS_HOME"
+echo "PATH: $PATH"
+echo
+grails -version
